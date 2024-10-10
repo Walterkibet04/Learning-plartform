6a52d15393a45e340ce918221fe51e3cc3963478
walter <kibetwalter726@gmail.com>
2024-10-10 15:00:29 +0300

    added urls and routing

--- chat/urls.py
+++ chat/urls.py
@@ -3,3 +3,10 @@ from . import views
 
 app_name = 'chat'
 
+urlpatterns = [
+    path(
+        'room/<int:course_id>/',
+        views.course_chat_room,
+        name='course_chat_room',
+    ),
+]
